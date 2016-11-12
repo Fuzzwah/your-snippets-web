f6ad6ba902a6a4f1cee851a8d84a499caa8ab6e6
Fuzzwah <rob.crouch@gmail.com>
2016-11-13 09:43:55 +1100

    fixing urls

--- your_snippets/api/urls.py
+++ your_snippets/api/urls.py
@@ -6,11 +6,11 @@ from your_snippets.api import views
 
 
 urlpatterns = [
-    url(r'^list/$', views.snippet_list),
-    url(r'^auth/$', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^list/', views.snippet_list),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
-    url(r'^add/$', views.snippet_add),
-    url(r'^add/images/$', views.images_add),
+    url(r'^add/', views.snippet_add),
+    url(r'^add/images/', views.images_add),
     url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail),
     url(r'^token/', token_views.obtain_auth_token),
 ]
