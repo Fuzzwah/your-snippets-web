08940633c9eba0c30245cbf3b16d9727eb15c78e
Rob Crouch <Rob.Crouch@utas.edu.au>
2016-11-14 13:49:52 +1100

    a clue!

--- your_snippets/api/urls.py
+++ your_snippets/api/urls.py
@@ -9,8 +9,8 @@ urlpatterns = format_suffix_patterns([
     url(r'^list/$', views.snippet_list, name='snippet-list'),
     url(r'^auth/$', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls, name='api-admin'),
+    url(r'^add/images/$', views.images_add, name='add-image'),
     url(r'^add/$', views.snippet_add, name='add-snippet'),
-    url(r'^images/$', views.images_add, name='add-image'),
     url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet-detail'),
     url(r'^token/$', token_views.obtain_auth_token, name='api-token'),
 ])
