ed6b16b2a70ccda76c09f0e023fe6169c4506f7c
Rob Crouch <Rob.Crouch@utas.edu.au>
2016-11-14 13:00:33 +1100

    no clue

--- your_snippets/api/urls.py
+++ your_snippets/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views as token_views
 from your_snippets.api import views
@@ -10,7 +11,7 @@ urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^add/', views.snippet_add),
-    #url(r'^add/images/', views.images_add),
+    url(r'^add/images/', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail),
     url(r'^token/', token_views.obtain_auth_token),
 ]
