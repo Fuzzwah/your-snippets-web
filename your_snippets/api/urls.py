5b71c3ff3da662d639dc2cab8a4f0df7260858bc
Rob Crouch <Rob.Crouch@utas.edu.au>
2016-11-14 13:43:36 +1100

    a clue!

--- your_snippets/api/urls.py
+++ your_snippets/api/urls.py
@@ -5,14 +5,14 @@ from rest_framework.authtoken import views as token_views
 from your_snippets.api import views
 
 
-urlpatterns = [
-    url(r'^list/$', views.snippet_list),
+urlpatterns = format_suffix_patterns([
+    url(r'^$', api_root),
+    url(r'^list/$', views.snippet_list, name='snippet-list'),
     url(r'^auth/$', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^add/$', views.snippet_add),
-    url(r'^add_images/$', views.images_add),
-    url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail),
-    url(r'^token/$', token_views.obtain_auth_token),
-]
+    url(r'^admin/', admin.site.urls, name='api-admin'),
+    url(r'^add/$', views.snippet_add, name='add-snippet'),
+    url(r'^images/$', views.images_add, name='add-image'),
+    url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet-detail'),
+    url(r'^token/$', token_views.obtain_auth_token, name='api-token'),
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
