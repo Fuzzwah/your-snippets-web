70d957842beb64928bdbd7c95992822431462d30
Rob Crouch <Rob.Crouch@utas.edu.au>
2016-11-15 14:38:35 +1100

    tags serializer

--- your_snippets/api/views.py
+++ your_snippets/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from your_snippets.api.models import Snippet
+from your_snippets.api.models import Snippet, Tag, SavedImage
 from your_snippets.api.serializers import (
     APIFullSerializer, SnippetCreateSerializer, ImageCreateSerializer, SnippetSerializer, ImageSerializer
 )
